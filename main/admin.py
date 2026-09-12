71e923b8a65d890dff7906552cfe632965af0da3
aryadwipa24 <arya.dwipa@ui.ac.id>
2026-09-12 18:40:34 +0700

    tugas 2: - mencoba membuat superuser admin untuk memudahkan update data

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Experience, Skill, Education
 # Register your models here.
+
+admin.site.register(Experience)
+admin.site.register(Skill)
+admin.site.register(Education)
