027f91e1e7af5e1cfa3b2c6cc3fcd4052e1b6eda
aryadwipa24 <arya.dwipa@ui.ac.id>
2026-09-12 18:46:00 +0700

    tugas 2: - menambahkan CSRF_TRUSTED_ORIGINS

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -152,3 +152,7 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://arya-dwipa-myportofolio.pws.cs.ui.ac.id",
+]
